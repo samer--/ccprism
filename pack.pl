6fbba4c64941fcd3ea2f20a1e72d9eee1bed5b5a
samer-- <s.abdallah@ucl.ac.uk>
2017-04-11 12:41:31 +0100

    Bump version

--- pack.pl
+++ pack.pl
@@ -1,5 +1,5 @@
 name(ccprism).
-version('0.0.4').
+version('0.0.5').
 author('Samer Abdallah','s.abdallah@ucl.ac.uk').
 title('Probabilistic programming using delimited continuations').
 download('https://github.com/samer--/ccprism.git').
