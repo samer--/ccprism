9a4861b4ac86ec0748de6371d38d1fa62a42f4a0
samer-- <samer.abdallah.00@gmail.com>
2017-09-09 15:19:09 +0100

    Add memoisation effect handler

--- examples/models.pl
+++ examples/models.pl
@@ -148,3 +148,20 @@ parse(NT,Words) :-
 :- cctable np2//0, nom2//0.
 np2 --> coin ~> c(the), nom2; c(a), nom2.
 nom2 --> die ~> c(cat); c(mat); c(dog); c(frog).
+
+% ----------- thread local memoisation for random world semantics ----------
+:- use_module(library(delimcc), [p_shift/2]).
+:- use_module(library(ccstate), [run_state_handler//3]).
+:- use_module(library(rbutils)).
+
+:- meta_predicate lazy(1,-).
+lazy(P,X) :- p_shift(memo,lazy(P,X)).
+
+run_memo(Goal) :- rb_empty(E), run_state_handler(memo,handle_memo,Goal,E,_).
+
+run_memo(Goal) --> run_state_handler(memo,handle_memo,Goal).
+handle_memo(lazy(P,X)) --> rb_get(P,X) -> []; run_memo(call(P,X)), rb_add(P,X).
+
+flip +-> [t,f].
+flip(X) :- lazy(:=(flip),X).
+two_flip(X,Y) :- flip(X), flip(Y).
