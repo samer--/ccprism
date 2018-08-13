dc3eebcc82f66305590237714a1e9baaf2b48ea6
samer-- <samer@jukedeck.com>
2018-08-13 14:21:46 +0100

    Add more testing code and more timing results

--- bench/harness.pl
+++ bench/harness.pl
@@ -1,4 +1,5 @@
 :- use_module(library(plrand)).
+:- use_module(library(listutils), [drop/3]).
 :- use_module(library(ccprism/handlers)).
 :- use_module(library(ccprism/learn)).
 :- use_module(library(ccprism)).
@@ -33,7 +34,7 @@ sample_and_learn_dice(Meth, K, N, H, P1, R1, R2) :-
 	run_sampling(S, maplist(dice(K),Xs), R1 ,R2),
    goal_graph(maplist(dice(K),Xs), G),
    graph_params(uniform, G, P0),
-   time(converge(abs(1e-4), learn(ml, io(Meth), G), H, P0, P1)).
+   time(converge(abs(1e-5), learn(ml, io(Meth), G), H, P0, P1)).
 
 init :-
    % set_prolog_flag(toplevel_mode, recursive),
@@ -43,4 +44,32 @@ init :-
 init_julia :-
    !using('Plots'),
    !unicodeplots(),
-   !default(show=true, size= #(900,400)).
+   !default(show=false, size= #(900,400)).
+
+add_plot(X, P1, P2) :- P2 ?? 'plot!'(P1, X).
+
+empty_plot(plot(xlabel="iteration", ylabel="log likelihood")).
+learn_history(Meth, K, N, H) --> sample_and_learn_dice(Meth, K, N, [_|H], _).
+
+with_die_sampler(Goal) -->
+   {make_lookup_sampler([(dice:die)-[0.2,0.4,0.1,0.3]], S)},
+	run_sampling(S, Goal).
+
+multitrial(Learner, K, N, Curves) :-
+   length(Xs,N), maplist(dice(K),Xs),
+   goal_graph(maplist(dice(K),Xs), G),
+   maplist(call(Learner, G), Curves).
+
+learn1(Modifier, Drop, Meth, G, H) :-
+   graph_params(random, G, P0),
+   converge(abs(1e-5), call(Modifier, learn(ml, io(Meth), G)), HFull, P0, _P1),
+   drop(Drop, HFull, H).
+
+multiplot(Curves, P1) :- empty_plot(P0), foldl(add_plot, Curves, P0, P1).
+
+with_plot(Setup, with_plot(Step)) :- call(Setup, Step).
+with_plot(Step, Cost, S1, S2) :-
+   call(Step, Cost, S1, S2),
+   member((dice:die)-Probs, S2),
+   format(string(Title), "~1f", [Cost]),
+   !gui(1, bar(Probs, title=Title, size= #(160, 160), ylim= #(0,0.6))).
--- bench/results.pl
+++ bench/results.pl
@@ -4,9 +4,26 @@ prism_settings(epsilon, 1.0e-6).
 
 % system, K, N, T_expl/ms, T_learn/ms, learn_its
 dice(prism, 10, 100,  1,  90,  2438).
+dice(prism, 10, 100,  1,  13,  332).
+dice(prism, 10, 100,  1,  66,  1671).
+dice(prism, 10, 100,  1, 116,  2708).
+dice(prism, 10, 100,  1,  50,  1256).
+dice(prism, 10, 100,  1,  46,  1248).
+
 dice(prism, 20, 100,  2, 171, 1410).
 dice(prism, 20, 100,  2, 452, 3289).
+dice(prism, 20, 100,  3, 687, 5323).
+dice(prism, 20, 100,  2, 400, 3070).
+dice(prism, 20, 100,  2, 617, 4553).
+dice(prism, 20, 100,  3, 1333, 10000).
+
 dice(prism, 10, 200,  1, 141, 3167).
+dice(prism, 10, 200,  1, 80, 1952).
+dice(prism, 10, 200,  1, 39, 973).
+dice(prism, 10, 200,  1, 7, 118).
+dice(prism, 10, 200,  1, 143, 3615).
+dice(prism, 10, 200,  1, 21, 515).
+
 dice(prism, 10, 500,  1,  49, 1195).
 dice(prism, 10, 500,  1,  69, 1785).
 dice(prism, 10, 500,  1, 147, 3302).
@@ -35,6 +52,41 @@ dice(ccp, 20, 100, 43, 76000, 3455).
 dice(ccp, 10, 500, 78, 3558, 493).
 dice(ccp, 10, 200, 34, 3824, 564).
 
+dice(Sys, A, B, C, 0, D, E) :- dice(Sys, A, B, C, D, E).
+
+dice(ccpws, 10, 100, 10, 214, 7377, 1507).
+dice(ccpws, 10, 100, 15, 250, 7667, 1507).
+dice(ccpws, 10, 100, 14, 255, 656, 125).
+dice(ccpws, 10, 100, 17, 252, 632, 125).
+% dice(ccpws, 10, 100, 15, 263, 3371, 670).
+% dice(ccpws, 10, 100, 15, 254, 3416, 670).
+dice(ccpws, 10, 100, 11, 259, 4771, 926).
+dice(ccpws, 10, 100, 17, 253, 4673, 926).
+
+dice(ccpws, 20, 100, 46, 1691, 19218, 945).
+dice(ccpws, 20, 100, 45, 1711, 21751, 945).
+dice(ccpws, 20, 100, 45, 1622, 64231, 3383).
+dice(ccpws, 20, 100, 32, 1648, 63885, 3383).
+% dice(ccpws, 20, 100, 45, 2797, 77503, 3825).
+% dice(ccpws, 20, 100, 46, 1757, 58808, 2889).
+dice(ccpws, 20, 100, 37, 1802, 4194, 202).
+dice(ccpws, 20, 100, 35, 1789, 4302, 202).
+
+dice(ccpws, 10, 200, 32, 264,  4301, 823).
+dice(ccpws, 10, 200, 34, 270, 4341, 823).
+dice(ccpws, 10, 200, 36, 259, 4908, 918).
+dice(ccpws, 10, 200, 16, 267, 4873, 918).
+dice(ccpws, 10, 200, 16, 248, 6224, 1189).
+dice(ccpws, 10, 200, 34, 263, 6352, 1189).
+
+summary(Sys, K, N, MeanExpl, MeanAD, MeanIt) :-
+   aggregate(r(bag(T_exp), bag(T_AD), bag(T_learn), bag(Its)), dice(Sys, K, N, T_exp, T_AD, T_learn, Its),
+             r(ExplTimes, ADTimes, LearnTimes, Its)),
+   mean(ExplTimes, MeanExpl),
+   mean(ADTimes, MeanAD),
+   sumlist(LearnTimes, LearnTime),
+   sumlist(Its, TotalIts),
+   MeanIt is LearnTime / TotalIts.
 
 summary(Sys, K, N, MeanExpl, MeanIt) :-
    aggregate(r(bag(T_exp), bag(T_learn), bag(Its)), dice(Sys, K, N, T_exp, T_learn, Its),
